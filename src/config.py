dc13ef7573dd89fe2f22c5b888971cf6838f39c9
Nikita Barsukov <n.v.barsukov@tinkoff.ru>
2022-12-20 21:28:02 +0400

    dev: add logging

---
+++ src/config.py
@@ -0,0 +1,4 @@
+from rich.console import Console
+
+log = Console().log
+rule = Console().rule
--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,14 @@
 import pandas as pd
 
+from src.config import log, rule
+
 if __name__ == "__main__":
-    # read data
+    log("Read data")
     train_df = pd.read_csv("./data/problem_train.csv", low_memory=False)
     test_df = pd.read_csv("./data/problem_test.csv", low_memory=False)
     train_labels_df = pd.read_csv("./data/problem_labels.csv")
 
-    # drop na
+    log("Find na cols")
     train_col_counts = train_df.describe(include="all").loc["count"]
     test_col_counts = test_df.describe(include="all").loc["count"]
 
@@ -14,8 +16,12 @@ if __name__ == "__main__":
     test_na_cols = test_col_counts[test_col_counts == 0].index.to_series()
     all_na_cols = pd.concat((train_na_cols, test_na_cols)).values
 
+    log("Drop na cols")
     train_df = train_df.drop(columns=all_na_cols)
     test_df = test_df.drop(columns=all_na_cols)
 
+    log("Save")
     train_df.to_csv("./data/train.csv")
     test_df.to_csv("./data/test.csv")
+
+    rule("Done")
