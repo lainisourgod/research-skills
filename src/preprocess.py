1d12af4e0479976d9b21083a444d7b3a098f0aad
Nikita Barsukov <n.v.barsukov@tinkoff.ru>
2022-12-20 22:33:15 +0400

    data: save labels, save to /prepared

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+
 import pandas as pd
 
 from src.config import log, rule
 
 if __name__ == "__main__":
     log("Read data")
-    train_df = pd.read_csv("./data/problem_train.csv", low_memory=False)
-    test_df = pd.read_csv("./data/problem_test.csv", low_memory=False)
-    train_labels_df = pd.read_csv("./data/problem_labels.csv")
+    raw_dir = Path("./data/raw")
+    train_df = pd.read_csv(raw_dir / "problem_train.csv", low_memory=False)
+    test_df = pd.read_csv(raw_dir / "problem_test.csv", low_memory=False)
+    train_labels_df = pd.read_csv(raw_dir / "problem_labels.csv")
 
     log("Find na cols")
     train_col_counts = train_df.describe(include="all").loc["count"]
@@ -21,7 +24,10 @@ if __name__ == "__main__":
     test_df = test_df.drop(columns=all_na_cols)
 
     log("Save")
-    train_df.to_csv("./data/train.csv")
-    test_df.to_csv("./data/test.csv")
+    prepared_dir = Path("./data/prepared")
+    prepared_dir.mkdir(exist_ok=True, parents=True)
+    train_df.to_csv(prepared_dir / "train.csv", index=False)
+    test_df.to_csv(prepared_dir / "test.csv", index=False)
+    train_labels_df.to_csv(prepared_dir / "train_labels.csv", index=False)
 
     rule("Done")
